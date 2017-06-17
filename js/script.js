a31bbfc1d2152b0e9e632f806647d7c3bba687de
Justis Corbett <corbettjustis@gmail.com>
2017-06-16 20:03:03 -0600

    finished layout, working on geolocation and ajax

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	var weatherSum;
+	var temperature;
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getWeather);
+        } else {
+            window.alert("Geolocation is not supported by this browser.");
+        }
+    }
+    function getWeather(position){
+        $.ajax({
+			url: 'https://api.darksky.net/forecast/d39b3dba17a4022bcddeb356e233c352/' + position.coords.latitude + ',' + position.coords.longitude + '?exclude=currently,minutely,daily,alerts,flags',
+					
+			dataType: 'json',
+			success: function ( data ) {
+                console.log(data);
+        		var post = data.shift(); // The data is an array of posts. Grab the first one.
+				var tempQuote = post.content;
+				quote = tempQuote.replace(/(<p[^>]+?>|<p>|<\/p>)/img, ""); //remove p tags
+				author = "&mdash; " + post.title;
+       			$( '#author' ).html( author );
+       			$( '#quotes' ).html( quote );
+				},
+				cache: false
+				});
+			};
+});
