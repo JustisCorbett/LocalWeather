416131f747d63cdb8a16336ee3028a39c3b8f9f1
Justis Corbett <corbettjustis@gmail.com>
2017-06-19 11:02:36 -0600

    api params

--- js/script.js
+++ js/script.js
@@ -11,14 +11,14 @@ $(document).ready(function(){
     function getWeather(position){
         console.log(position);
         $.ajax({
-			url: 'https://thingproxy.freeboard.io/fetch/https://api.darksky.net/forecast/d39b3dba17a4022bcddeb356e233c352/' + position.coords.latitude + ',' + position.coords.longitude + '?exclude=hourly,minutely,daily,alerts,flags',
+			url: 'https://thingproxy.freeboard.io/fetch/https://api.darksky.net/forecast/d39b3dba17a4022bcddeb356e233c352/' + position.coords.latitude + ',' + position.coords.longitude + '?exclude=currently,minutely,daily,alerts,flags',
 					
 			dataType: 'json',
 			success: function ( data ) {
                 console.log(data);
-        		var post = data.shift(); // The data is an array of posts. Grab the first one.
-				var tempQuote = post.content;
-				quote = tempQuote.replace(/(<p[^>]+?>|<p>|<\/p>)/img, ""); //remove p tags
+        		var post = data.shift(1); // The data is an array of posts. Grab the first one.
+				var tempWeath = post.content;
+				quote = tempWeath.replace(/(<p[^>]+?>|<p>|<\/p>)/img, ""); //remove p tags
 				author = "&mdash; " + post.title;
        			$( '#author' ).html( author );
        			$( '#quotes' ).html( quote );
